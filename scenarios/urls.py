c24ed47ae67eb7e81081f3ce5227304374421c1d
Seth Hill <seth@pointnineseven.com>
2014-09-22 10:39:43 -0700

    Imports MP-135

--- scenarios/urls.py
+++ scenarios/urls.py
@@ -1,11 +1,16 @@
-from django.conf.urls.defaults import *
-from views import *
+from django.conf.urls import patterns, url
+from views import (sdc_analysis, delete_design, get_attributes, get_scenarios,
+                   get_leaseblocks, get_leaseblock_features, get_sharing_groups,
+                   share_design, copy_design, get_selections) 
 
 urlpatterns = patterns('',
-    #feature reports
-    url(r'sdc_report/(\d+)', sdc_analysis, name='sdc_analysis'), #user requested sdc analysis 
-    url(r'delete_design/(?P<uid>[\w_]+)/$', delete_design), #user deletes scenario (or cancels empty geometry result)
-    url(r'get_attributes/(?P<uid>[\w_]+)/$', get_attributes), #get attributes for a given scenario
+    # feature reports
+    # user requested sdc analysis
+    url(r'sdc_report/(\d+)', sdc_analysis, name='sdc_analysis'),
+    # user deletes scenario (or cancels empty geometry result)
+    url(r'delete_design/(?P<uid>[\w_]+)/$', delete_design), 
+    # get attributes for a given scenario
+    url(r'get_attributes/(?P<uid>[\w_]+)/$', get_attributes), 
     url(r'get_scenarios$', get_scenarios),
     url(r'get_leaseblocks$', get_leaseblocks),
     url(r'get_sharing_groups$', get_sharing_groups),
