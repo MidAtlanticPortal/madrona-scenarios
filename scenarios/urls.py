7ba5102d1c53b11e869ba210e4058543a591583e
Seth Hill <seth@pointnineseven.com>
2015-07-07 11:53:27 -0700

    MP-942 Add export code

--- scenarios/urls.py
+++ scenarios/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
 from views import (sdc_analysis, delete_design, get_attributes, get_scenarios,
                    get_leaseblocks, get_leaseblock_features,
-                   share_design, copy_design, get_selections) 
+                   share_design, copy_design, get_selections, ExportShapefile,
+                   ExportGeoJSON, ExportWKT, ExportKML)
 
 urlpatterns = patterns('',
     # feature reports
@@ -16,5 +17,14 @@ urlpatterns = patterns('',
     url(r'share_design$', share_design),
     url(r'copy_design/(?P<uid>[\w_]+)/$', copy_design),
     url(r'get_selections$', get_selections),
-    url(r'get_leaseblock_features$', get_leaseblock_features)
+    url(r'get_leaseblock_features$', get_leaseblock_features),
+
+    url(r'export/shp/(?P<feature_id>[\w_]+).zip$',
+        ExportShapefile.as_view(), name='export_shp'),
+    url(r'export/geojson/(?P<feature_id>[\w_]+).geojson$',
+        ExportGeoJSON.as_view(), name='export_geojson'),
+    url(r'export/wkt/(?P<feature_id>[\w_]+)-wkt.txt',
+        ExportWKT.as_view(), name='export_wkt'),
+    url(r'export/kml/(?P<feature_id>[\w_]+).kml',
+        ExportKML.as_view(), name='export_kml'),
 )
