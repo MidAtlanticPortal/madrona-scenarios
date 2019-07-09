220d4fbcab3c23fe0416fdf4719a8514b5611be8
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-07-09 15:29:00 -0700

    django 2 url patterns update

--- scenarios/urls.py
+++ scenarios/urls.py
@@ -1,30 +1,33 @@
-from django.conf.urls import patterns, url
+# from django.conf.urls import patterns, url
+from django.urls import re_path, include
+# from . import views
 from .views import (sdc_analysis, delete_design, get_attributes, get_scenarios,
                    get_leaseblocks, get_leaseblock_features,
                    share_design, copy_design, get_selections, ExportShapefile,
                    ExportGeoJSON, ExportWKT, ExportKML)
 
-urlpatterns = patterns('',
+urlpatterns = [
+    #'',
     # feature reports
     # user requested sdc analysis
-    url(r'sdc_report/(\d+)', sdc_analysis, name='sdc_analysis'),
+    re_path(r'sdc_report/(\d+)', sdc_analysis, name='sdc_analysis'),
     # user deletes scenario (or cancels empty geometry result)
-    url(r'delete_design/(?P<uid>[\w_]+)/$', delete_design), 
+    re_path(r'delete_design/(?P<uid>[\w_]+)/$', delete_design),
     # get attributes for a given scenario
-    url(r'get_attributes/(?P<uid>[\w_]+)/$', get_attributes), 
-    url(r'get_scenarios$', get_scenarios),
-    url(r'get_leaseblocks$', get_leaseblocks),
-    url(r'share_design$', share_design),
-    url(r'copy_design/(?P<uid>[\w_]+)/$', copy_design),
-    url(r'get_selections$', get_selections),
-    url(r'get_leaseblock_features$', get_leaseblock_features),
+    re_path(r'get_attributes/(?P<uid>[\w_]+)/$', get_attributes),
+    re_path(r'get_scenarios$', get_scenarios),
+    re_path(r'get_leaseblocks$', get_leaseblocks),
+    re_path(r'share_design$', share_design),
+    re_path(r'copy_design/(?P<uid>[\w_]+)/$', copy_design),
+    re_path(r'get_selections$', get_selections),
+    re_path(r'get_leaseblock_features$', get_leaseblock_features),
 
-    url(r'export/shp/(?P<feature_id>[\w_]+).zip$',
+    re_path(r'export/shp/(?P<feature_id>[\w_]+).zip$',
         ExportShapefile.as_view(), name='export_shp'),
-    url(r'export/geojson/(?P<feature_id>[\w_]+).geojson$',
+    re_path(r'export/geojson/(?P<feature_id>[\w_]+).geojson$',
         ExportGeoJSON.as_view(), name='export_geojson'),
-    url(r'export/wkt/(?P<feature_id>[\w_]+)-wkt.txt',
+    re_path(r'export/wkt/(?P<feature_id>[\w_]+)-wkt.txt',
         ExportWKT.as_view(), name='export_wkt'),
-    url(r'export/kml/(?P<feature_id>[\w_]+).kml',
+    re_path(r'export/kml/(?P<feature_id>[\w_]+).kml',
         ExportKML.as_view(), name='export_kml'),
-)
+]
