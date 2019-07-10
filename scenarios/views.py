8a59c9f0ac55e3def16fbb0d22c24d4ae750de9b
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-07-10 13:07:02 -0700

    I think this is just different line endings

--- scenarios/views.py
+++ scenarios/views.py
@@ -25,7 +25,7 @@ def sdc_analysis(request, sdc_id):
     if not viewable:
         return response
     return display_sdc_analysis(request, sdc_obj)
-    
+
 
 @csrf_exempt
 def copy_design(request, uid):
@@ -33,16 +33,16 @@ def copy_design(request, uid):
         design_obj = get_feature_by_uid(uid)
     except Feature.DoesNotExist:
         raise Http404
-       
+
     #check permissions
     viewable, response = design_obj.is_viewable(request.user)
     if not viewable:
         return response
-        
+
     design_obj.pk = None
     design_obj.user = request.user
     design_obj.save()
-    
+
     json = []
     json.append({
         'id': design_obj.id,
@@ -51,9 +51,9 @@ def copy_design(request, uid):
         'description': design_obj.description,
         'attributes': design_obj.serialize_attributes()
     })
-    
+
     return HttpResponse(dumps(json), status=200)
-    
+
 
 @csrf_exempt
 def delete_design(request, uid):
@@ -61,22 +61,22 @@ def delete_design(request, uid):
         design_obj = get_feature_by_uid(uid)
     except Feature.DoesNotExist:
         raise Http404
-    
+
     #check permissions
     viewable, response = design_obj.is_viewable(request.user)
     if not viewable:
         return response
-        
+
     design_obj.delete()
     #design_obj.active = False
     #design_obj.save(rerun=False)
-    
+
     return HttpResponse("", status=200)
 
 @login_required()
 def get_scenarios(request):
     json = []
-    
+
     scenarios = Scenario.objects.filter(user=request.user, active=True).order_by('date_created')
     for scenario in scenarios:
         # Allow for "sharing groups" without an associated MapGroup, for "special" cases
@@ -91,7 +91,7 @@ def get_scenarios(request):
             'attributes': scenario.serialize_attributes(),
             'sharing_groups': sharing_groups
         })
-        
+
     shared_scenarios = Scenario.objects.shared_with_user(request.user)
     for scenario in shared_scenarios:
         if scenario.active and scenario not in scenarios:
@@ -107,7 +107,7 @@ def get_scenarios(request):
                 'shared_by_username': username,
                 'shared_by_name': actual_name
             })
-        
+
     return HttpResponse(dumps(json))
 
 @login_required()
@@ -126,7 +126,7 @@ def get_selections(request):
             'attributes': selection.serialize_attributes(),
             'sharing_groups': sharing_groups
         })
-        
+
     shared_selections = LeaseBlockSelection.objects.shared_with_user(request.user)
     for selection in shared_selections:
         if selection not in selections:
@@ -142,11 +142,11 @@ def get_selections(request):
                 'shared_by_username': username,
                 'shared_by_name': actual_name
             })
-        
-    return HttpResponse(dumps(json))    
-    
+
+    return HttpResponse(dumps(json))
+
 '''
-'''    
+'''
 def get_leaseblock_features(request):
     srid = settings.GEOJSON_SRID
     leaseblock_ids = request.GET.getlist('leaseblock_ids[]')
@@ -169,31 +169,31 @@ def get_leaseblock_features(request):
         '''
         #json.append({'type': "Feature", 'geometry': leaseblock.geometry.geojson, 'properties': {}})
     #return HttpResponse(dumps(json[0]))
-    geojson = """{ 
+    geojson = """{
       "type": "FeatureCollection",
       "crs": { "type": "name", "properties": {"name": "%s"}},
-      "features": [ 
-      %s 
+      "features": [
+      %s
       ]
     }""" % (srid_to_urn(srid), ', \n'.join(feature_jsons),)
     return HttpResponse(geojson)
-    
+
 
 def get_attributes(request, uid):
     try:
         scenario_obj = get_feature_by_uid(uid)
     except Scenario.DoesNotExist:
         raise Http404
-    
+
     #check permissions
     viewable, response = scenario_obj.is_viewable(request.user)
     if not viewable:
         return response
-    
+
     return HttpResponse(dumps(scenario_obj.serialize_attributes()))
 
 '''
-'''    
+'''
 @csrf_exempt
 def share_design(request):
     group_names = request.POST.getlist('groups[]')
@@ -209,7 +209,7 @@ def share_design(request):
     groups = [g.map_group.permission_group for g in groups]
     design.share_with(groups, append=False)
     return HttpResponse("", status=200)
-    
+
 '''
 '''
 @cache_page(60 * 60 * 24, key_prefix="scenarios_get_leaseblocks")
@@ -240,7 +240,7 @@ def get_leaseblocks(request):
             #'tsz_mean_distance': ocs_block.tsz_mean_distance,
             #'wea_name': ocs_block.wea_name,
             #'wea_number': ocs_block.wea_number,
-            #'wea_state_name': ocs_block.wea_state_name  
+            #'wea_state_name': ocs_block.wea_state_name
             'uxo': ocs_block.uxo
         })
     return HttpResponse(dumps(json))
@@ -373,5 +373,3 @@ class ExportKML(GeometryExporter):
         response['Content-Disposition'] = 'attachment; filename=%s.kml' % feature.name
 
         return response
-
-
